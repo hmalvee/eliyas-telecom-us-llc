14d18685aa398f1c88e4b382b2406b13d102e7bc
hmalvee <info.alvee392@gmail.com>
2025-05-25 03:27:39 -0700

    Fix toast import in AppContext

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState } from 'react';
-import { toast } from 'react-hot-toast';
+import { toast } from 'react-toastify';
 import { Invoice } from '../types';
 
 interface AppContextType {
